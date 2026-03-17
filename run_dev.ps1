3de0291178dc2a3cd576597297c4c9afbbd3b5b8
ansyar <m.ansyarafi@gmail.com>
2026-03-17 21:43:34 +1100

    feat(workflow): Add run_dev.ps1 and run_tests.ps1 for automation

---
+++ run_dev.ps1
@@ -0,0 +1,5 @@
+$env:PYTHONPATH = "."
+$env:TCL_LIBRARY = "C:\Users\Ansyar\AppData\Local\Programs\Python\Python313\tcl\tcl8.6"
+$env:TK_LIBRARY = "C:\Users\Ansyar\AppData\Local\Programs\Python\Python313\tcl\tk8.6"
+.venv\Scripts\Activate.ps1
+python src\main.py
